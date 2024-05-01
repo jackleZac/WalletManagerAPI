b9e5b7536c993c0342aa4138e844f23ea2e7c650
root <root@isaac>
2024-05-01 15:18:37 +0800

    Initial version

---
+++ expense.py
@@ -0,0 +1,110 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from bson import ObjectId
+import pymongo
+import datetime
+import os 
+
+
+
+app = Flask(__name__)
+
+# Load config from .env file
+load_dotenv()
+MONGODB_URI = os.environ['MONGODB_URI']
+
+def connect_to_db():
+    # Set up a connection to MongoDB cluster
+    try:
+        client = pymongo.MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
+        if client.server_info():
+            print('SUCCESS: MongoDB is connected!')
+            # Getting a database
+            db = client['myfinance']
+            # Getting a collection
+            collection = db['expense']
+            return collection
+        else:
+            print('ERROR: MongoDB is not connected!')
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print('ERROR: MongoDB connection timed out!')
+    except Exception as e:
+        print('ERROR: An unexpected error occurrred: {e}')
+    
+expense_collection = connect_to_db()
+
+############################################################################################
+#####                         ADD FUNCTIONS HERE                                      ######
+############################################################################################
+
+@app.route('/expense', methods=['POST'])
+def add_expense():
+    """It should add an expense to database"""
+    # Receive parsed data sent from the front-end (React)
+    expense = request.json
+    # Parse ISO 8601 formatted date string from the request
+    iso_date_string = expense["date"]
+    date = datetime.datetime.strptime(iso_date_string, "%Y-%m-%dT%H:%M:%S.%f")
+    # Update the format of expense date
+    expense["date"] = date
+    # Insert an expense into MongoDB Atlas
+    expense_collection.insert_one(expense)
+    # Return a success message
+    return jsonify({"Message": "An expense has been succesfully added"}), 201
+
+@app.route('/expense', methods=['GET'])
+def get_expenses():
+    """It should return a list of all available expenses"""
+    # Get a list of expenses from MongoDB
+    list_of_expenses = expense_collection.find({})
+    # Convert the ObjectId instances to a JSON serializable format
+    expenses = [
+        {"_id": str(expense["_id"]), "amount": expense["amount"], "date": expense["date"], 
+         "category": expense["category"],"description": expense["description"], "repeatMonthly": expense["repeatMonthly"]} 
+        for expense in list_of_expenses
+    ]
+    # Return a JSON document to the front-end
+    return jsonify({'expenses': expenses})
+
+@app.route('/expense/<string:_id>', methods=["PUT"])
+def update_expense(_id):
+    """It should update an expense"""
+    # Get a content of updated expense
+    updated_expense = request.json
+    # Parse ISO 8601 formatted date string from the request
+    iso_date_string = updated_expense["date"]
+    date = datetime.datetime.strptime(iso_date_string, "%Y-%m-%dT%H:%M:%S.%f")
+    # Update the format of expense date
+    updated_expense["date"] = date
+    # Log the received ID for debugging
+    app.logger.debug(f"Received ID: {_id}")
+    # Find and update the expense in MongoDB
+    response = expense_collection.find_one_and_update(
+        {"_id": ObjectId(_id)},
+        {"$set": updated_expense} )
+    if response is None:
+        # An expense with the specified id is not found
+        return jsonify({"message": f'Expense with id: {_id} is not found'}), 404
+    else:
+        # An expense is found and updated
+        return jsonify({"message": f'Expense with id: {_id} is updated'}), 200
+
+@app.route('/expense/<string:_id>', methods=["DELETE"])
+def delete_expense(_id):
+    """It should delete an expense"""
+    # Find and delete an expense from MongoDB
+    response = expense_collection.find_one_and_delete({"_id": ObjectId(_id)})
+    if response is None:
+        # An expense is not found hence causing failure to delete
+        return jsonify({"message": f'Failed to delete expense with id: {_id}'}), 404
+    else:
+        # An expense is found and deleted
+        return jsonify({"message": f'Expense with id: {_id} is deleted'}), 200
+
+if __name__ == '__main__':   
+    app.run(debug=True)
+    
+
+
+        
+
---
+++ test/test_expense.py
@@ -0,0 +1,139 @@
+import unittest
+import sys
+import datetime
+import json
+import dotenv
+
+# Add parent directory to Python path
+sys.path.append('../')
+from expense import app, connect_to_db
+
+class TestExpenses(unittest.TestCase):
+    """Test cases for expenses operations"""
+    def setUp(self):
+        # Create a connection to MongoDB Atlas 
+        self.db = connect_to_db()
+        # Initialize test client to simulate requests to Flask App
+        self.app = app.test_client()
+        
+    def tearDown(self):
+        # Clean up all resources in database
+        self.db.delete_many({})
+        
+    
+    def test_add_expense(self):
+        """It should add an expense and assert that it exists"""
+        expense_to_be_added = { "amount": 70.00, "date": datetime.datetime.now().isoformat(), "category": "Fitness", "description": "A Monthly Payment for Eagle Gym Membership", "repeatMonthly": True }
+        # Make a POST request to def add_expense()
+        response = self.app.post('/expense', json=expense_to_be_added)
+        # Fetch the expense from MongoDB atlas
+        expense_from_database = self.db.find_one({"description": expense_to_be_added["description"]})
+        # Assert that an expense has been created
+        self.assertEqual(response.status_code, 201)
+        self.assertIsNotNone(expense_from_database)
+        # Assert that the details are accurate
+        self.assertEqual(expense_from_database["amount"], expense_to_be_added["amount"])
+        self.assertEqual(expense_from_database["category"], expense_to_be_added["category"])
+        self.assertEqual(expense_from_database["description"], expense_to_be_added["description"])
+        self.assertEqual(expense_from_database["repeatMonthly"], expense_to_be_added["repeatMonthly"])
+        
+    def test_list_expense(self):
+        """It should get all expenses from database"""
+        # Create a list of THREE expenses
+        expenses_to_be_added = [
+            {"amount": 70.00, "date": datetime.datetime.now().isoformat(), "category": "Fitness", "description": "A Monthly Payment for Eagle Gym Membership", "repeatMonthly": True },
+            {"amount": 50.00, "date": datetime.datetime.now().isoformat(), "category": "Meals", "description": "Lunch at McDonald's", "repeatMonthly": False },
+            {"amount": 40.00, "date": datetime.datetime.now().isoformat(), "category": "Car", "description": "Paid Gas", "repeatMonthly": False }, ]
+        # Insert the list of expenses into MongoDB 
+        self.db.insert_many(expenses_to_be_added)
+        # Make a GET request to def get_expenses()
+        response = self.app.get('/expense')
+        # Assert that it has been successfully retrieved
+        self.assertEqual(response.status_code, 200)
+        # Convert JSON data to Python dict
+        response_dict = json.loads(response.data)
+        # Assert that the number of expenses returned is equal to the number of test expenses
+        self.assertEqual(len(response_dict['expenses']), len(expenses_to_be_added))
+        # Assert that the expenses returned are similar to the test expenses
+        found = False
+        for test_expense in expenses_to_be_added:
+            for expense in response_dict["expenses"]:
+                if expense["description"] == test_expense["description"]:
+                    found = True
+                    break
+            self.assertTrue(found)
+            
+    def test_update_expense(self):
+        """It should update an expense from a database"""
+        expense_to_be_added = {
+            "amount": 70.00, 
+            "date": datetime.datetime.now().isoformat(), 
+            "category": "Fitness", 
+            "description": "A Monthly Payment for Eagle Gym Membership", 
+            "repeatMonthly": True} 
+        # Insert an expense into MongoDB 
+        insert_expense = self.db.insert_one(expense_to_be_added)
+        self.assertTrue(insert_expense.acknowledged)
+        # Proceed when expense has been successfully inserted
+        if (insert_expense):
+            # Retrieve expense from database
+            test_expense = self.db.find_one({"description": expense_to_be_added["description"]})
+            # Assert that the expense exists
+            self.assertIsNotNone(test_expense)
+            print(test_expense)
+            # Assert that the expense ID is not None
+            self.assertIsNotNone(test_expense["_id"])
+            print(test_expense["_id"])
+            test_expense_id = test_expense["_id"]
+            # Make changes to an expense amount, date and description
+            expense_updated = {
+                "amount": 241.00, # Update price
+                "date": datetime.datetime.now().isoformat(), # Update date
+                "category": "Fitness", # category remains unchanged
+                "description": "A Monthly Payment for BJJ Membership", # Update description
+                "repeatMonthly": True} # repeatMonthly remains unchanged
+            # Make a PUT request to def update_expense()
+            response = self.app.put(f'/expense/{test_expense_id}', json=expense_updated, content_type='application/json')
+            # Assert that the expense has been successfully updated
+            self.assertEqual(response.status_code, 200)
+            # Fetch the expense from MongoDB atlas
+            expense_from_database = self.db.find_one({"_id": test_expense_id})
+            # Assert that an expense exists
+            self.assertIsNotNone(expense_from_database)
+            # Assert that an expense has been updated
+            self.assertEqual(expense_from_database["amount"], expense_updated["amount"])
+            self.assertEqual(expense_from_database["description"], expense_updated["description"])
+        else:
+            # Raise an error if expense was not inserted
+             self.fail("Failed to insert expense into database")
+             
+    def test_delete_expense(self):
+        """It should delete an expense"""
+        expense_to_be_added = {
+            "amount": 70.00, 
+            "date":datetime.datetime.now().isoformat(), 
+            "category": "Fitness", 
+            "description": "A Monthly Payment for Eagle Gym Membership", 
+            "repeatMonthly": True} 
+        # Insert an expense into MongoDB 
+        insert_expense = self.db.insert_one(expense_to_be_added)
+        self.assertTrue(insert_expense.acknowledged)
+        # Retrieve expense from database
+        test_expense = self.db.find_one({"description": expense_to_be_added["description"]})
+        # Assert that the expense exists
+        self.assertIsNotNone(test_expense)
+        # Assert that the expense ID is not None
+        self.assertIsNotNone(test_expense["_id"])
+        test_expense_id = test_expense["_id"]
+        # Make a DELETE request to def delete_expense()
+        response = self.app.delete(f'/expense/{test_expense_id}')
+        # Assert that the expense has been successfully updated
+        self.assertEqual(response.status_code, 200)
+        # Make an attempt to fetch the expense again
+        deleted_expense = self.db.find_one({"_id": test_expense_id})
+        # Assert that the expense is not found
+        self.assertIsNone(deleted_expense)
+            
+    
+if __name__=='__main__':
+    unittest.main()

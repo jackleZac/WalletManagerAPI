b194b13bc1ef0f57471d76b7301cd99e4654e093
root <root@isaac>
2024-05-04 17:58:12 +0800

    Update app.py app.run(host='0.0.0.0', port=5000)

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def delete_expense(_id):
         return jsonify({"message": f'Expense with id: {_id} is deleted'}), 200
 
 if __name__ == '__main__':   
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
 
 
